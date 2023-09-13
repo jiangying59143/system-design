a27427549a50acc975ed379c936a713ab415eaa3
907292671@qq.com <510050274!@qaz>
2023-09-13 18:31:40 +0800

    system design - rabbitmq

---
+++ mq_consumer/src/main/java/org/example/mq_consumer/MqConsumerApplication.java
@@ -0,0 +1,11 @@
+package org.example.mq_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MqConsumerApplication.class, args);
+    }
+}
