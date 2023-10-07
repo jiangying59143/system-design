2388b779e1f311e38e2fab6f53f3bf2e12eb6745
jiangying-Lenovo <907292671@qq.com>
2023-10-08 00:18:48 +0800

    system design - algorithm

--- algorithm/src/main/java/org/example/algorithm/Leetcode655.java
+++ algorithm/src/main/java/org/example/algorithm/Leetcode655.java
@@ -3,48 +3,44 @@ package org.example.algorithm;
 import org.example.algorithm.dataStructure.TreeNode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Leetcode655 {
 
     public List<List<String>> printTree(TreeNode root) {
-        List<List<String>> list = new ArrayList<>();
-        int height = getDepth(root)-1, r = getRows(height), c = getColumns(height);
-        for(int i=0; i<r; i++){
+        List<List<String>> res = new ArrayList<>();
+        int height = getHeight(root), m = getRows(height), n = getColumns(height);
+        for(int i=0; i<m; i++){
             List<String> subList = new ArrayList<>();
-            for(int j=0; j<c; j++){
+            for(int j=0; j<n; j++){
                 subList.add("");
             }
+            res.add(subList);
         }
-        process(root, 0, list);
-        return list;
+        process(root, height, 0,  (n-1)/2, res);
+        return res;
     }
 
-    private int getDepth(TreeNode node){
-        if(node == null){
-            return 0;
-        }
-        return Math.max(getDepth(node.left)+1, getDepth(node.right)+1);
-    }
-
-    private void process(TreeNode node, int height, List<List<String>> list){
+    private void process(TreeNode node, int height, int r, int c, List<List<String>> res){
         if(node == null){
             return;
         }
-        process(node.left, height+1, list);
-        process(node.right, height+1, list);
-
-
+        res.get(r).set(c, String.valueOf(node.val));
+        process(node.left, height,  r+1, getColumnIndex(height, r, c, true), res);
+        process(node.right, height, r+1,getColumnIndex(height, r, c, false), res);
     }
 
-    private int getRowIndex(int height){
-        int r  = getRows(height)-1;
-        return r+1;
+    private int getHeight(TreeNode node){
+        if(node == null){
+            return -1;
+        }
+        return Math.max(getHeight(node.left)+1, getHeight(node.right)+1);
     }
 
-    private int getColumnIndex(int height){
-        int c = getColumns(height)-1,r  = getRows(height)-1;;
-        return c - (int)(Math.pow(2, height-r-1));
+    private int getColumnIndex(int height, int r, int c, boolean isLeft){
+        if(isLeft) return c - (int)(Math.pow(2, height-r-1));
+        return c + (int)(Math.pow(2, height-r-1));
     }
 
     private int getRows(int height){
@@ -53,5 +49,28 @@ public class Leetcode655 {
 
     private int getColumns(int height){
         return (int)(Math.pow(2, height+1))-1;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(new Leetcode655().getColumnIndex(2, 0, 3, true));
+        TreeNode root;
+        List<List<String>> res;
+
+        root = new TreeNode(1);
+        root.left(2);
+        res = new Leetcode655().printTree(root);
+        print(res);
+
+        root = new TreeNode(1);
+        root.left(2).right(4);root.right(3);
+        res = new Leetcode655().printTree(root);
+        print(res);
+
+    }
+
+    private static void print(List<List<String>> res ){
+        for (List<String> re : res) {
+            System.out.println(Arrays.toString(re.toArray()));
+        }
     }
 }
