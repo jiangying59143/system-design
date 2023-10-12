aa20a339c34c0f293631f42bc82f352d10d75632
jiangying-Lenovo <907292671@qq.com>
2023-10-12 23:12:14 +0800

    system design - algorithm

--- algorithm/src/main/java/org/example/algorithm/Leetcode572.java
+++ algorithm/src/main/java/org/example/algorithm/Leetcode572.java
@@ -3,6 +3,8 @@ package org.example.algorithm;
 import org.example.algorithm.dataStructure.TreeNode;
 
 public class Leetcode572 {
+
+    //todo 超出时间限制
     public static boolean isSubtree(TreeNode root, TreeNode subRoot) {
         return process(root, subRoot, subRoot);
     }
@@ -14,8 +16,8 @@ public class Leetcode572 {
         if(root == null || subRoot == null){
             return false;
         }
-        return root.val == subRoot.val && isSubtree(root.left, subRoot.left) && isSubtree(root.right, subRoot.right)
-                || isSubtree(root.left, origin) || isSubtree(root.right, origin);
+        return root.val == subRoot.val && process(root.left, subRoot.left, origin) && process(root.right, subRoot.right, origin)
+                || process(root.left, origin, origin) || process(root.right, origin, origin);
     }
 
     public static void main(String[] args) {
@@ -29,6 +31,12 @@ public class Leetcode572 {
 
         subRoot = new TreeNode(4);subRoot.left(1); subRoot.right(2);
         root = new TreeNode(3);TreeNode left = root.left(4);left.left(1); left.right(2).left(0); root.right(5);
+        TreeNode.printTree(root);
+        TreeNode.printTree(subRoot);
+        System.out.println(isSubtree(root,subRoot));
+
+        subRoot = new TreeNode(3);subRoot.left(1);subRoot.right(2);
+        root = new TreeNode(3);root.left(4).left(1);root.right(5).left(2);
         TreeNode.printTree(root);
         TreeNode.printTree(subRoot);
         System.out.println(isSubtree(root,subRoot));
