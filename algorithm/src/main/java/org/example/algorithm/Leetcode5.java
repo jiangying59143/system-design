9505a0ce65bb8e4b47e148e1d4d1d27904477205
jiangying-Lenovo <907292671@qq.com>
2023-10-17 20:51:34 +0800

    system design - algorithm

--- algorithm/src/main/java/org/example/algorithm/Leetcode5.java
+++ algorithm/src/main/java/org/example/algorithm/Leetcode5.java
@@ -16,6 +16,55 @@ public class Leetcode5 {
         return s.substring(pos[0], pos[1]+1);
     }
 
+    public static String longestPalindrome1(String s){
+        if(s == null || s.length() <= 1){
+            return s;
+        }
+        s = getManacherStr(s);
+
+        int C = 0, R = -1, start = 0, end = 0,len, maxLen = 1;
+        int[] parr = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            if(R >= i){
+                int initArmLen = Math.min(parr[2*C-i], R - i);
+                len = expandToMax(s, i-initArmLen, i+initArmLen);
+            }else{
+                len = expandToMax(s, i, i);
+            }
+
+            parr[i] = len/2;
+
+            if(i + len/2 > R){
+                C = i;
+                R = i + len/2;
+            }
+
+            if(len > maxLen){
+                maxLen = len;
+                start = i - len/2;
+                end = i + len/2;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = start; i <= end; i++) {
+            if(s.charAt(i) != '#'){
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    private static String getManacherStr(String s){
+        StringBuilder sb = new StringBuilder();
+        sb.append("#");
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(s.charAt(i));
+            sb.append("#");
+        }
+        return sb.toString();
+    }
+
     private static int expandToMax(String s, int i, int j){
         while(i >=0 && j < s.length() && s.charAt(i) == s.charAt(j)){
             i--; j++;
@@ -24,7 +73,18 @@ public class Leetcode5 {
     }
 
     public static void main(String[] args) {
-        System.out.println(longestPalindrome("babad"));
-        System.out.println(longestPalindrome("cbbd"));
+        String s;
+
+        s = "babad";
+        System.out.println("原 string : " + s);
+        System.out.println("中心扩展方法 ： " + longestPalindrome(s));
+        System.out.println("manacher方法 ： " + longestPalindrome1(s));
+        System.out.println("-----------------------------------------");
+
+        s = "cbbd";
+        System.out.println("原 string : " + s);
+        System.out.println("中心扩展方法 ： " + longestPalindrome(s));
+        System.out.println("manacher方法 ： " + longestPalindrome1(s));
+        System.out.println("-----------------------------------------");
     }
 }
