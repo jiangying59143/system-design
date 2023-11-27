29a40322acc8646b311e21b1ba6e7ce8f3638201
jiangying-Lenovo <907292671@qq.com>
2023-11-27 22:41:29 +0800

    system design - algorithm

--- algorithm/src/main/java/org/example/algorithm/Leetcode337.java
+++ algorithm/src/main/java/org/example/algorithm/Leetcode337.java
@@ -1,61 +1,36 @@
 package org.example.algorithm;
 
 import org.example.algorithm.dataStructure.TreeNode;
-import org.example.algorithm.view.PrettyPrintTree;
 
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Leetcode337 {
-    static class MyTreeNode extends TreeNode{
-        public MyTreeNode left;
-        public MyTreeNode right;
-        public int val;
-        public int ans = -1;
 
-        public MyTreeNode(int val) {
-            super(val);
-            this.val = val;
-        }
-
-        public static void printTree(MyTreeNode tree){
-            var pt = new PrettyPrintTree<MyTreeNode>(
-                    (x) -> Arrays.asList(x.left, x.right),
-                    (x) -> String.valueOf(x.val)
-            );
-            pt.display(tree);
-            System.out.println("--------------------------------------------------");
-        }
-    }
+    private Map<TreeNode, Integer> f = new HashMap<>(), g =  new HashMap<>();
     public int rob(TreeNode root) {
         if(root == null) return 0;
-        MyTreeNode mRoot = new MyTreeNode(root.val);
-        copyTree(root, mRoot);
-        MyTreeNode.printTree(mRoot);
-        return process(mRoot, false);
-    }
-    private void copyTree(TreeNode node, MyTreeNode mNode){
-        if(node == null) return;
-        if(node.left != null) {
-            mNode.left = new MyTreeNode(node.left.val);
-        }
-        if(node.right != null) {
-            mNode.right = new MyTreeNode(node.right.val);
-        }
-        copyTree(node.left, mNode.left);
-        copyTree(node.right, mNode.right);
+//        return process(root, false);
+        process(root);
+        return Math.max(f.getOrDefault(root, 0), g.getOrDefault(root, 0));
     }
 
-    private int process(MyTreeNode node, boolean flag){
+    private int process(TreeNode node, boolean flag){
         if(node == null) return 0;
-//        if(node.ans != -1) return node.ans;
         int ans = process(node.left, false) + process(node.right, false);
         if(!flag){
             ans = Math.max(ans,
                     node.val + process(node.left, true) + process(node.right, true));
         }
-        node.ans = ans;
         return ans;
+    }
+
+    private void process(TreeNode node){
+        if(node== null) return;
+        process(node.left);
+        process(node.right);
+        g.put(node, node.val + f.getOrDefault(node.left, 0) + f.getOrDefault(node.right, 0));
+        f.put(node, Math.max(f.getOrDefault(node.left, 0), g.getOrDefault(node.left, 0)) + Math.max(f.getOrDefault(node.right, 0), g.getOrDefault(node.right, 0)));
     }
 
 
