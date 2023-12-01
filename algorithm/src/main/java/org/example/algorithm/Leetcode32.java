9425d4b851c84a14af4510e7977589d8b1a833a8
907292671@qq.com <510050274!@qaz>
2023-12-01 16:46:01 +0800

    System Design - Algorithm leetcode

--- algorithm/src/main/java/org/example/algorithm/Leetcode32.java
+++ algorithm/src/main/java/org/example/algorithm/Leetcode32.java
@@ -11,7 +11,7 @@ public class Leetcode32 {
         for (int i = 1; i < s.length(); i++) {
             if(')' == s.charAt(i)){
                 if('(' == s.charAt(i-1)) dp[i] = 2 + (i-2< 0 ? 0 : dp[i-2]);
-                else if(i-dp[i-1]-1 >= 0 && '('  == s.charAt(i-dp[i-1]-1)) dp[i] = dp[i-dp[i-1]-1] + 2 + dp[i-1];
+                else if(i-dp[i-1] > 1 && '('  == s.charAt(i-dp[i-1]-1)) dp[i] = (i-dp[i-1]-2 < 0 ? 0 : dp[i-dp[i-1]-2]) + 2 + dp[i-1];
                 max = Math.max(max, dp[i]);
 //                else dp[i] = dp[i-1];
             }
@@ -42,25 +42,63 @@ public class Leetcode32 {
         return ret;
     }
 
+    // 贪心算法
+    public int longestValidParentheses3(String s) {
+        int left = 0, right = 0, ret = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') left++;
+            else right++;
+
+            if(left == right) ret = Math.max(ret, left + right);
+            else if(right > left) left = right = 0;
+        }
+        left = right = 0;
+        for (int r = s.length()-1; r >= 0; r--) {
+            if(s.charAt(r) == '(') left++;
+            else right++;
+
+            if(left == right) ret = Math.max(ret, left + right);
+            else if(left > right) left = right = 0;
+        }
+        return ret;
+    }
+
     public static void main(String[] args) {
         String s;
 
-//        s = "(()";
-//        System.out.println(new Leetcode32().longestValidParentheses1(s));
-//        System.out.println(new Leetcode32().longestValidParentheses2(s));
-//        System.out.println();
-//        s = ")()())";
-//        System.out.println(new Leetcode32().longestValidParentheses1(s));
-//        System.out.println(new Leetcode32().longestValidParentheses2(s));
-//        System.out.println();
-//        s = "())";
-//        System.out.println(new Leetcode32().longestValidParentheses1(s));
-//        System.out.println(new Leetcode32().longestValidParentheses2(s));
-//        System.out.println();
+        s = "(()";
+        System.out.println(s);
+        System.out.println(new Leetcode32().longestValidParentheses1(s));
+        System.out.println(new Leetcode32().longestValidParentheses2(s));
+        System.out.println(new Leetcode32().longestValidParentheses3(s));
+        System.out.println();
+
+        s = ")()())";
+        System.out.println(s);
+        System.out.println(new Leetcode32().longestValidParentheses1(s));
+        System.out.println(new Leetcode32().longestValidParentheses2(s));
+        System.out.println(new Leetcode32().longestValidParentheses3(s));
+        System.out.println();
+
+        s = "())";
+        System.out.println(s);
+        System.out.println(new Leetcode32().longestValidParentheses1(s));
+        System.out.println(new Leetcode32().longestValidParentheses2(s));
+        System.out.println(new Leetcode32().longestValidParentheses3(s));
+        System.out.println();
 
         s = "()(()";
+        System.out.println(s);
         System.out.println(new Leetcode32().longestValidParentheses1(s));
-//        System.out.println(new Leetcode32().longestValidParentheses2(s));
+        System.out.println(new Leetcode32().longestValidParentheses2(s));
+        System.out.println(new Leetcode32().longestValidParentheses3(s));
+        System.out.println();
+
+        s = "()(())";
+        System.out.println(s);
+        System.out.println(new Leetcode32().longestValidParentheses1(s));
+        System.out.println(new Leetcode32().longestValidParentheses2(s));
+        System.out.println(new Leetcode32().longestValidParentheses3(s));
         System.out.println();
 
     }
