8a06ddba1777600caf63a598ddd42faeea3a8f30
yjiang <907292671@qq.com>
2024-03-19 12:09:35 +0800

    add repo

--- docker/mysql/master/init/init.sql
+++ docker/mysql/master/init/init.sql
@@ -2,12 +2,12 @@ CREATE USER 'master-slave1'@'master-slave1' IDENTIFIED BY 'master-slave1';
 grant replication slave, replication client on *.* to 'master-slave1'@'master-slave1';
 CREATE USER 'master-slave2'@'master-slave2' IDENTIFIED BY 'master-slave2';
 grant replication slave, replication client on *.* to 'master-slave2'@'master-slave2';
--- CREATE USER 'canal'@'172.18.0.5' IDENTIFIED BY 'canal';
--- grant select, replication slave, replication client on *.* to 'canal'@'172.18.0.5';
+-- CREATE USER 'canal'@'canal' IDENTIFIED BY 'canal';
+-- grant select, replication slave, replication client on *.* to 'canal'@'canal';
 FLUSH PRIVILEGES;
 
--- SHOW GRANTS FOR 'master1-slave1'@'172.18.0.3';
--- SHOW GRANTS FOR 'master1-slave2'@'172.18.0.4';
--- SHOW GRANTS FOR 'canal'@'172.18.0.5';
+-- SHOW GRANTS FOR 'master-slave1'@'master-slave1';
+-- SHOW GRANTS FOR 'master-slave2'@'master-slave2';
+-- SHOW GRANTS FOR 'canal'@'canal';
 --
 -- SHOW MASTER STATUS;
