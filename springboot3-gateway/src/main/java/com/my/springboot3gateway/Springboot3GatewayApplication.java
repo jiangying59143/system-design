a8ca833cbbbc6c9ab17d84a3fcfa7ad82d008dc9
影魔 <907292671@qq.com>
2024-05-23 21:47:02 +0800

    springboot oauth2 client change a little

---
+++ springboot3-gateway/src/main/java/com/my/springboot3gateway/Springboot3GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.my.springboot3gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3GatewayApplication.class, args);
+    }
+
+}
